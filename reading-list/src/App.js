e1c2662e6d55ef1a17266966f6916e6de51fb323
Patrycja Kubińska <kubinskapatrycja@gmail.com>
2021-06-02 11:09:20 +0200

    Add static model.

--- reading-list/src/App.js
+++ reading-list/src/App.js
@@ -1,10 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import { TrashOutline } from "react-ionicons";
 
-function App() {
-  return (
-  
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="list">
+          <h1>Lista książek do przeczytania</h1>
+          <div className="list-wrapper">
+            <ul>
+              <li>
+                <input type="checkbox" />
+                <div className="title">Tytuł</div>
+                <div className="opis">Opis</div>
+                <TrashOutline color={"#00000"} height="25px" width="25px" />
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="form">
+          <h1>Dodaj nową książkę</h1>
+          <label>
+            Książka
+            <input type="text" />
+          </label>
+          <label>
+            Dlaczego warto przeczytać?
+            <textarea />
+          </label>
+          <button> Dodaj </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
