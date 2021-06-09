606114454d108b784f67571dec07ee26f2839747
Patrycja Kubińska <kubinskapatrycja@gmail.com>
2021-06-09 16:59:15 +0200

    useEffect

--- rts-learn/src/index.tsx
+++ rts-learn/src/index.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import EventComponent from "./events/EventComponent";
 import { GuestList } from "./state/GuestList";
-import UserSearch from "./state/UserSearch";
+import UserSearch from "./refs/UserSearch";
 
 const App = () => {
   return (
     <div>
-      <EventComponent />
+      <UserSearch />
     </div>
   );
 };
