26c4470566cf34db17c0bdd667d3f7a4baaa528f
Patrycja Kubińska <kubinskapatrycja@gmail.com>
2021-06-09 14:32:16 +0200

    Handling drag event

--- rts-learn/src/events/EventComponent.tsx
+++ rts-learn/src/events/EventComponent.tsx
@@ -1,10 +1,15 @@
 const EventComponent: React.FC = () => {
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => console.log(e);
 
+  const onDragStart = (event: React.DragEvent<HTMLDivElement>) =>
+    console.log(event);
   return (
     <div>
       <input onChange={(e) => console.log(e)}></input>
       <input onChange={onChange}></input>
+      <div draggable onDragStart={onDragStart}>
+        Drag me?
+      </div>
     </div>
   );
 };
