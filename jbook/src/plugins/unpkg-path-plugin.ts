ee8002123ed3776af26db2cae00b52b4bffd2548
Patrycja Kubińska <kubinskapatrycja@gmail.com>
2021-06-15 14:11:22 +0200

    Impl a Caching Layer & Cach with Key-Value Pairs

--- jbook/src/plugins/unpkg-path-plugin.ts
+++ jbook/src/plugins/unpkg-path-plugin.ts
@@ -1,5 +1,17 @@
 import * as esbuild from "esbuild-wasm";
 import axios from "axios";
+import localForage from "localforage";
+
+const fileCache = localForage.createInstance({
+  name: "filecache",
+});
+
+(async () => {
+  await fileCache.setItem("color", "red");
+
+  const color = await fileCache.getItem("color");
+  console.log(color);
+})();
 
 export const unpkgPathPlugin = () => {
   return {
@@ -40,12 +52,29 @@ export const unpkgPathPlugin = () => {
           };
         }
 
+        // Check to see if we have alredy fetched this file
+        // and if it is in the cache
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+
+        //if it is, return immediately
+        if (cachedResult) {
+          return cachedResult;
+        }
+
         const { data, request } = await axios.get(args.path);
-        return {
+
+        const result: esbuild.OnLoadResult = {
           loader: "jsx",
           contents: data,
           resolveDir: new URL("./", request.responseURL).pathname,
         };
+
+        //store response in cache
+        await fileCache.setItem(args.path, result);
+
+        return result;
       });
     },
   };
